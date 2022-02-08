62a7c4afcfe158240a241718187188f4bd8088f2
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-08 16:22:17 -0700

    wrote fetch utils

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,20 @@
+import { client, checkError } from './client';
+
+export async function getPokemons(from = 0, to = 30) {
+  const response = await client
+    .from('pokemons')
+    .select()
+    .range(from, to);
+
+  return checkError(response);
+}
+
+export async function getSinglePokemon(id) {
+  const response = await client
+    .from('pokemons')
+    .select()
+    .match({ id })
+    .single();
+
+ return checkError(response);
+}
