ac8ded0330bf71d9b2aeaef56eecabf143778612
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-08 17:54:45 -0700

    finished writing pokemonsPage, list of pokemon rendering

--- src/PokemonsPage.js
+++ src/PokemonsPage.js
@@ -5,11 +5,29 @@ import PokemonList from './PokemonList';
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
+  const [page, setPage] = useState(1);
+  const perPage = 50;
 
+  useEffect(() => {
+    async function fetch() {
+      const from = page * perPage - perPage;
+      const to = page * perPage;
+      const pokes = await getPokemons(from, to);
+
+      setPokemons(pokes);
+    }
+    fetch();
+  }, [page]);
 
   return (
     <>
-      <div>PokemonsPage</div>
+      <h3>Current Page {page}</h3>
+      <div>
+        <button onClick={() => setPage(page - 1)}
+          disabled={page === 1}>Previous Page</button>
+        <button onClick={() => setPage(page + 1)}
+          disabled={pokemons.length < perPage}>Next Page</button>
+      </div>
       <PokemonList pokemons={pokemons} />
     </>
   );
