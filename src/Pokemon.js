415ff0d13059062fe5bffd159d57cca92efc7fef
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-10 17:44:47 -0700

    refactored a couple componants without issue, pokemonDetail only one that broke

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,12 +1,28 @@
 import { Link } from 'react-router-dom';
 
-export default function Pokemon({ pokemon }) {
-  return (
-    <Link to={`/pokemons/${pokemon.id}`}>
-      <div className='pokemon'>
-        <img className='pokemon-img' src={pokemon.url_image}/>
-        <p>{pokemon.pokemon}</p>
-      </div>
-    </Link>
-  );
+// export default function Pokemon({ pokemon }) {
+//   return (
+//     <Link to={`/pokemons/${pokemon.id}`}>
+//       <div className='pokemon'>
+//         <img className='pokemon-img' src={pokemon.url_image}/>
+//         <p>{pokemon.pokemon}</p>
+//       </div>
+//     </Link>
+//   );
+// }
+
+import React, { Component } from 'react';
+
+export default class Pokemon extends Component {
+  render() {
+    const { pokemon } = this.props;
+    return (
+      <Link to={`/pokemons/${pokemon.id}`}>
+        <div className='pokemon'>
+          <img className='pokemon-img' src={pokemon.url_image}/>
+          <p>{pokemon.pokemon}</p>
+        </div>
+      </Link>
+    );
+  }
 }
--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,14 +1,32 @@
 import Pokemon from './Pokemon.js';
 
-export default function PokemonList({ pokemons }) {
-  return (
-    <div className='pokemon-list'>
-      {
-        pokemons.map(pokemon => <Pokemon key={pokemon.id, pokemon.pokemon} 
-          pokemon={pokemon}
-        />)
+// export default function PokemonList({ pokemons }) {
+//   return (
+//     <div className='pokemon-list'>
+//       {
+//         pokemons.map(pokemon => <Pokemon key={pokemon.id, pokemon.pokemon} 
+//           pokemon={pokemon}
+//         />)
 
-      }
-    </div>);
+//       }
+//     </div>);
   
+// }
+
+import React, { Component } from 'react';
+
+export default class PokemonList extends Component {
+  render() {
+    const { pokemons } = this.props;
+    return (
+      <div className='pokemon-list'>
+        {
+          pokemons.map(pokemon => <Pokemon key={pokemon.id, pokemon.pokemon} 
+            pokemon={pokemon}
+          />)
+
+        }
+      </div>);
+    
+  }
 }
